59dac0bb8bb7feffd31f22f3aac0be012ea452f1
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-15 00:06:53 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'pupa-pink': '#FF69B4',
+        'lupa-blue': '#00BFFF',
+      },
+    },
+  },
+  plugins: [],
+}
